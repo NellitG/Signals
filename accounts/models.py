49ab85f0c814c0f869fcbb16b320cad4c72c32f8
NellitG <kamaunelson157@gmail.com>
2025-08-05 14:49:35 +0300

    Create contact model with name, email and message input

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"
+    
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,)
     bio = models.TextField(blank=True, null=True)
