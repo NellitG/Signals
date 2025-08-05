f6bc243908e0406cc2110100db7290f6eef253da
NellitG <kamaunelson157@gmail.com>
2025-08-05 14:00:46 +0300

    Add signal receiver to add, delete user profile on users

--- accounts/signal.py
+++ accounts/signal.py
@@ -4,6 +4,14 @@ from .models import UserProfile
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.db.models.signals import pre_delete
+from django.db.models.signals import post_delete
+from django.db.models.signals import pre_save
+
+@receiver(pre_save, sender=User)
+def pre_save_user_profile(sender, instance, **kwargs):
+    print(f"Preparing to save user profile for {instance.username}.")
+    instance.userprofile.save()
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -27,3 +35,13 @@ def save_user_profile(sender, instance, **kwargs):
 def delete_user_profile(sender, instance, **kwargs):
     print (f"Good Bye {instance.username}!! We are deleting your account.")
     instance.userprofile.delete()
+
+@receiver(post_delete, sender=User)
+def delete_user_profile(sender, instance, **kwargs):
+    print(f"Account for {instance.username} has been deleted.")
+    
+    send_mail(
+        subject='Account Deletion Confirmation',
+        message=f'Hi {instance.username}, your account has been successfully deleted.',
+    ) 
+
