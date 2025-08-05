06384b64e9f16dfb5c47aedd04ca3bbdfbaada68
NellitG <kamaunelson157@gmail.com>
2025-08-05 14:52:25 +0300

    Add contact view function to handle GET and POST requests for contact page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'accounts/contact.html')
+    else:
+        form = ContactForm()
+
+    return render(request, 'accounts/contact.html', {'form': form})
